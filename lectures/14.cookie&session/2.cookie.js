db07509b2043db1582947b4e05984c093f665c57
zhangrenyang-t510 <zhang_renyang@>
2015-11-29 16:35:50 +0800

    session

--- lectures/14.cookie&session/2.cookie.js
+++ lectures/14.cookie&session/2.cookie.js
@@ -1,26 +1,12 @@
 /**
- * cookie 是web服务器向浏览器发送的一段ASCII文本
- * 客户端一旦收到cookie,浏览器会很开心的保存在本地 key=value
- * 以后每次客户端向服务器发请求，都需要把之前发给它的cookie发回给服务器
- *
+  1.一旦服务器向客户端发了cookie,以后除非 cookie过期，否则 浏览器每次都会向服务器发cookie
+  2.存放在客户端，容易被篡改
+  -------------------------------
+ 1.不要存储私密数据
+ 2.正确的设置path domain 以减少cookie的发送
+ 3. 正确的设置httpOnly防止cookie被篡改
  */
-/**
- * 设置cookie的时候还需要设置一些额外的参数
- *  Set-Cookie: name=zfpx; path=/foo; domain=.baidu.com;
- *  Key=Value
- *    名称值对。这个是必须的
- *  path
- *    控制访问哪些路径可以发送cookie.
- *  domain
- *    指定cookie会发送到哪个域名下面
- *  expires
- *  max-age
- *    指定cookie的失效时间，如果没有指定失效时间，那么cookie不会写入硬盘，只持续到会话结束(浏览器关闭)
- *  httpOnly
- *    不能在js里操作cookie
- *
- * @type {exports|module.exports}
- */
+
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
@@ -30,7 +16,7 @@ http.createServer(function(request,response){
     if('/favicon.ico' == pathname){
         return response.end('404');
     }else if(pathname == '/write'){//写cookie
-        response.writeHead(200,{'Content-Type':'text/html',"Set-Cookie":["name=zfpx","age=6"]});
+        response.writeHead(200,{'Content-Type':'text/html',"Set-Cookie":["name=zfpx","isAdmin=0"]});
         //response.setHeader("Set-Cookie","name=zfpx; path=/a");
         //response.setHeader("Set-Cookie","name=zfpx; domain=b.zfpx.cn");
         //response.setHeader("Set-Cookie","name=zfpx; Expires="+new Date(new Date().getTime()+3600*1000).toGMTString());
@@ -40,11 +26,13 @@ http.createServer(function(request,response){
     }else{//读cookie
         // name=zfpx; age=6; hobby=smoking
         // name=zfpx&age=6
+        //document.cookie="isAdmin=1"
         var cookie = request.headers.cookie;
         var cookieObj = querystring.parse(cookie,'; ');
         response.setHeader('Content-Type',"text/html;charset=utf-8");
         if(cookieObj.name){
-            response.end('欢迎你老朋友');
+            var isAdmin = cookieObj.isAdmin==1?"管理员":"普通用户";
+            response.end('欢迎你老朋友,你是'+isAdmin);
         }else{
             response.end('欢迎你新朋友');
         }
