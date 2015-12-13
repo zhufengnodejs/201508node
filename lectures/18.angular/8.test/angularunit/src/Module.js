4cef50fc0db0191ecdeb46aaba1d536eafcb6ce0
zhangrenyang-t510 <zhang_renyang@>
2015-12-13 17:15:13 +0800

    单元测试

--- lectures/18.angular/8.test/angularunit/src/Module.js
+++ lectures/18.angular/8.test/angularunit/src/Module.js
@@ -1,4 +1,22 @@
 angular.module('testModule',['ngResource','ngMock']);
-angular.module('testModule').controller('HomeCtrl',function($scope){
+angular.module('testModule').controller('HomeCtrl',function($scope,UserFactory){
     $scope.title = '珠峰培训';
+    $scope.users = UserFactory.query();
+});
+
+angular.module('testModule').factory('UserFactory',function($resource){
+    return $resource('User/users.json');
+});
+
+angular.module('testModule').directive('sayHello',function(){
+    return {
+        replace:true,
+        template:'<h1>hello</h1>'
+    }
+});
+
+angular.module('testModule').filter('bigger',function(){
+    return function(input){
+        return input + 'bigger';
+    }
 });
--- lectures/18.angular/8.test/angularunit/test/Module.spec.js
+++ lectures/18.angular/8.test/angularunit/test/Module.spec.js
@@ -1,13 +1,59 @@
 (function(){
     describe('HomeCtrl',function(){
-       var $scope;
+       var $scope;//声明一个变量
         beforeEach(module('testModule'));//构建模块
         beforeEach(inject(function($rootScope,$controller){
-            $scope = $rootScope.$new();
+            $scope = $rootScope.$new();//创建一个新的scope
             $controller('HomeCtrl',{$scope:$scope});
         }));
         it('正确的title',inject(function(){
             expect($scope.title == '珠峰培训').toBeTruthy();
         }));
+    });
+    //测试指令
+    describe('HomeCtrl',function(){
+        var $compile;// 编辑服务
+        var $rootScope;//根作用域
+        var $httpBackend;// 模拟后台服务
+        beforeEach(module('testModule'));//构建模块
+        //注入的时候找服务器的会把下划线去掉
+        beforeEach(inject(function(_$compile_,_$rootScope_,_$httpBackend_){
+            $compile = _$compile_;
+            $rootScope =_$rootScope_;
+            $httpBackend = _$httpBackend_;
+            $httpBackend.when('POST','/users/validate').respond({username:'张三'});
+        }));
+        it('指令内容是否进行了真正的替换',function(){
+            var element = $compile('<say-hello></say-hello>')($rootScope);
+            $rootScope.$digest();//触发所有监听
+            console.log(element.html());
+            expect(element.html()).toContain('hello');
+        });
+    });
+    //测试过滤 器
+    describe('测试过滤器',function(){
+        beforeEach(module('testModule'));//构建模块
+        var bigger;
+        beforeEach(inject(function($filter){
+            bigger = $filter('bigger');
+        }));
+        it('add bigger',function(){
+            expect(bigger('original')).toEqual('originalbigger');;
+        });
+    });
+
+    describe('测试http',function(){
+        beforeEach(module('testModule'));//构建模块
+       var scope,$httpBackend;
+        beforeEach(inject(function($controller,$rootScope,_$httpBackend_){
+            $httpBackend = _$httpBackend_;
+            $httpBackend.when('GET','User/users.json').respond([{username:'张三'},{username:'李四'}]);
+            scope = $rootScope.$new();
+            $controller('HomeCtrl',{$scope:scope});
+        }));
+        it('应该返回2个用户',function(){
+            $httpBackend.flush();// 模拟后台返回响应
+            expect(scope.users.length).toBe(2);
+        });
     });
 })();
